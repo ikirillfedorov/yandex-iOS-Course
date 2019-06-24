eedce5bc64764e5a9951c2d7128d8c0d72f7b769
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-06-24 23:28:21 +0300

    initial commit

---
+++ Notes/Notes/Note.swift
@@ -0,0 +1,34 @@
+//
+//  Note.swift
+//  test
+//
+//  Created by Kirill Fedorov on 23/06/2019.
+//  Copyright © 2019 Kirill Fedorov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Importance: String {
+    case unimportant = "unimportant"
+    case ordinary = "ordinary"
+    case important = "important"
+}
+
+struct Note {
+    let uid: String
+    let title: String
+    let content: String
+    let noteColor: UIColor
+    let importance: Importance
+    let destractionDate: Date?
+    
+    init(uid: String?, title: String, content: String, noteColor: UIColor?, importance: Importance, destractionDate: Date?) {
+        self.uid = uid ?? UUID().uuidString
+        self.title = title
+        self.content = content
+        self.noteColor = noteColor ?? .white
+        self.importance = importance
+        self.destractionDate = destractionDate
+    }
+}
---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Kirill Fedorov on 24/06/2019.
+//  Copyright © 2019 Kirill Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
