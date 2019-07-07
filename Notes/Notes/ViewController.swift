80f9e6bbf04cdcb1663faa90e05815b74e8d95eb
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-07-07 16:33:35 +0300

    Add HW 3rd block w/o color picker

--- Notes/Notes/ViewController.swift
+++ Notes/Notes/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IB Outlets
+    @IBOutlet var colorViews: [CheckMarkView]!
+    @IBOutlet weak var contentTextView: UITextView!
     @IBOutlet weak var topConstrain: NSLayoutConstraint!
     @IBOutlet weak var bottomConstrain: NSLayoutConstraint!
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    //MARK: - IB Actions
     @IBAction func datePiackerSwitchAction(_ sender: UISwitch) {
         if sender.isOn {
             datePicker.isHidden = false
@@ -25,10 +30,35 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func tappedToMainScrollView(_ sender: UITapGestureRecognizer) {
+        self.contentTextView.resignFirstResponder() //turn off keyboard from contentTextView
+        let point = sender.location(in: self.view)
+        guard let tappedView = self.view.hitTest(point, with: nil) else { return }
         
-        
+        if tappedView is CheckMarkView {
+            for view in colorViews {
+                view.isHasCheckMark = view == tappedView
+                view.setNeedsDisplay()
+            }
+        }
+    }
+
+    //MARK: - Supported functions
+    func setting() {
+        colorViews[0].isHasCheckMark = true //default check mark
+        guard let image = UIImage(named: "pallete.jpeg") else { return } //get pallette image
+        colorViews[3].backgroundColor = UIColor.init(patternImage: image) //set color for multiColor cell
+        for view in colorViews {
+            view.layer.borderColor = UIColor.black.cgColor
+            view.layer.borderWidth = 2
+        }
+        contentTextView.layer.borderColor = UIColor.gray.cgColor
+        contentTextView.layer.borderWidth = 1
     }
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setting()
+    }
 }
